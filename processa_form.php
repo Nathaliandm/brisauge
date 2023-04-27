7d8f98d34bd763f2d6717b7990246a07b898fc06
Nathaliandm <131887555+Nathaliandm@users.noreply.github.com>
2023-04-27 17:23:57 -0300

    Update and rename cmp.php to processa_form.php

---
+++ processa_form.php
@@ -0,0 +1,15 @@
+<php
+// Recupera os dados do formulário
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+
+// Valida os dados do formulário
+if (empty($nome) || empty($email)) {
+  // Se algum campo estiver vazio, exibe uma mensagem de erro
+  echo 'Por favor, preencha todos os campos.';
+} else {
+  // Se os dados estiverem corretos, envia o formulário e redireciona o usuário
+  mail('seuemail@example.com', 'Novo formulário enviado', "Nome: $nome\nEmail: $email");
+  header('Location: confirmacao.html');
+}
+>
